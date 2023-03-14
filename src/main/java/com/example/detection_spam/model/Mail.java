db1a65fcdf1ad3a5bbf7d7a574d2a3d5b2068eb0
r.palayret <palayrer@3il.fr>
2023-03-14 09:22:47 +0100

    Add exemple javadoc

--- src/main/java/com/example/detection_spam/model/Mail.java
+++ src/main/java/com/example/detection_spam/model/Mail.java
@@ -1,4 +1,54 @@
+/*
+ * Application Detection Spam                                                                       14/03/2023
+ * Groupe Romain PALAYRET, Alexis RAVAYROL, Marin BAILHE, Jérôme CHIROL
+ * 3il Ingenieur Rodez
+ */
 package com.example.detection_spam.model;
 
+/**
+ * Objet java qui represente un mail et qui permet de faire des opérations avec
+ * @author Alexis RAVAYROL, Romain PALAYRET
+ * @version 1.0.0
+ */
 public class Mail {
+
+    private String contenu;
+
+    // private Etat
+
+    /**
+     * Constructeur de la classe Mail
+     * @param contenu le texte du mail
+     */
+    public Mail(String contenu) {
+
+    }
+
+    /**
+     * Renvoie le texte associé au mail, donc son contenu
+     * @return le contenu du mail
+     */
+    public String getText() {
+        return null;
+    }
+
+//    /**
+//     * Modifie l'etat du mail (SPAM, NON_SPAM, NON_TRAITE)
+//     * @param etat le nouvel etat du mail
+//     */
+//    public setEtat(Etat etat) {
+//
+//    }
+
+//    /**
+//     * Renvoie l'etat du mail (SPAM, NON_SPAM, NON_TRAITE)
+//     * @return l'etat du mail
+//     */
+//    public Etat getEtat() {
+//        return null;
+//    }
+//
+
+
+
 }
