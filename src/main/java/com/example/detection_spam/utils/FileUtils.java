f115b1ef7e03e9599d07fa8e0bb75f3773718636
m.bailhe <biadzok4@gmail.com>
2023-03-14 12:01:16 +0100

    fin du cours, tests à faire sur parsefile

--- src/main/java/com/example/detection_spam/utils/FileUtils.java
+++ src/main/java/com/example/detection_spam/utils/FileUtils.java
@@ -1,29 +1,49 @@
 package com.example.detection_spam.utils;
 
 import com.example.detection_spam.model.Mail;
+
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class FileUtils {
-    public Mail parseFile(String filePath) {
-        Mail mail = new Mail();
-
-
-        return mail;
+    public static Mail parseFile(String filePath) {
+        if (true) {//Pattern.matches("\\.txt", filePath)
+            Path P1 = Paths.get(filePath);
+            try(BufferedReader br = Files.newBufferedReader(P1)){
+                String lines = br.readLine();
+                while(lines != null){
+                    System.out.println(lines);
+                    lines = br.readLine();
+                }
+                return new Mail(lines);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 
 
-    public ArrayList<Mail> parseFolder(String folderPath) {
+    public static ArrayList<Mail> parseFolder(String folderPath) {
         ArrayList<Mail> mails = new ArrayList<Mail>();
         File folder = new File(folderPath);
-        File[] files = folder.listFiles();
-        for (File file : files) {
-            if (file.isFile()) {
-                Mail mail = parseFile(file.getPath());
-                mails.add(mail);
+        if (folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            for (File file : files) {
+                if (file.isFile()) {
+                    Mail mail = parseFile(file.getPath());
+                    mails.add(mail);
+                }
             }
+            return mails;
         }
-        return mails;
+        else
+            return null;
     }
 }
--- src/main/java/com/example/detection_spam/utils/test/TestFileUtils.java
+++ src/main/java/com/example/detection_spam/utils/test/TestFileUtils.java
@@ -1,4 +1,24 @@
+/*
+ * Application Detection Spam                                                                       14/03/2023
+ * Groupe Romain PALAYRET, Alexis RAVAYROL, Marin BAILHE, Jérôme CHIROL
+ * 3il Ingenieur Rodez
+ */
+
 package com.example.detection_spam.utils.test;
 
+import com.example.detection_spam.model.Mail;
+import com.example.detection_spam.utils.FileUtils;
+
 public class TestFileUtils {
+    /**
+     * TODO méthode qui test le constructeur de la class dictionnaire
+     */
+    private static void testParseFile() {
+        Mail mailtest = FileUtils.parseFile("C:\\Users\\m.bailhe\\IdeaProjects\\Detection_Spam\\src\\main\\resources\\Data\\Test.txt");
+        System.out.println(mailtest.getText());
+    }
+
+    public static void main (String[] args) {
+        testParseFile();
+    }
 }
