71a1e4726f69eb236687865c705a378faac5611f
biadzok <biadzok4@gmail.com>
2023-03-24 09:24:25 +0100

    récupération complète du travail perdu

--- src/main/java/com/example/detection_spam/utils/EmailUtils.java
+++ src/main/java/com/example/detection_spam/utils/EmailUtils.java
@@ -32,20 +32,38 @@ public class EmailUtils {
         return folderList;
     }
 
+    private static String getcontentFromMultipartMail(MimeMultipart mpt) throws MessagingException, IOException {
+        String res = "";
+        for (int i = 0; i < mpt.getCount(); i++) {
+            BodyPart bodyPart = mpt.getBodyPart(i);
+            if (bodyPart.isMimeType("text/plain")) {
+                res += "\n" + bodyPart.getContent();
+            }
+        }
+        return res;
+    }
+
     public static ArrayList<Mail> ParseEmail(Folder[] folders) throws MessagingException, IOException {
         ArrayList<Mail> mails = new ArrayList<Mail>();
 
         for (Folder folder : folders) {
             if (folder.getMessageCount() != 0) {
+                folder.open(Folder.READ_ONLY);
                 for (int i = 1; i < folder.getMessageCount(); i++) {
                     Message message = folder.getMessage(i);
                     String content = message.getSubject();
-                    Multipart mpt = (Multipart) message.getContent();
-
+                    if (message.isMimeType("TEXT/plain")) {
+                        content += message.getContent().toString();
+                    }
+                    else {
+                        MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+                        content += getcontentFromMultipartMail(mimeMultipart);
+                    }
 
                     Mail mail = new Mail(content);
                     mails.add(mail);
                 }
+                folder.close();
             }
         }
         return mails;
--- src/main/java/com/example/detection_spam/utils/test/TestEmailUtils.java
+++ src/main/java/com/example/detection_spam/utils/test/TestEmailUtils.java
@@ -6,6 +6,7 @@ import com.example.detection_spam.utils.FileUtils;
 import javax.mail.MessagingException;
 import java.util.ArrayList;
 
+import static com.example.detection_spam.utils.EmailUtils.ParseEmail;
 import static com.example.detection_spam.utils.EmailUtils.emailConnect;
 
 /**
@@ -24,9 +25,21 @@ public class TestEmailUtils {
 
     }
 
+    private static void testParseMails() {
+        try {
+            ArrayList<Mail> mails = ParseEmail(emailConnect());
+            for(Mail mail : mails) {
+                System.out.println(mail.getText());
+                System.out.println("\n*******************************\n");
+            }
+        }catch(Exception e){
+            System.err.println(e);
+        }
+    }
+
 
     public static void main (String[] args){
-        testConnexion();
+        testParseMails();
 
     }
 }
