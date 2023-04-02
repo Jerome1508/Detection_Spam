80865ff97873f222314fca07ab7e6016fb924679
AlexisR3il <ravayroa@3il.fr>
2023-04-02 11:29:11 +0200

    SonarLint : TestMail

--- src/main/java/com/example/detection_spam/model/test/TestMail.java
+++ src/main/java/com/example/detection_spam/model/test/TestMail.java
@@ -14,25 +14,27 @@ import com.example.detection_spam.model.Mail;
  * @version 1.0.0
  */
 public class TestMail {
+    static final String testContent = "Ceci est un test";
+    static final String testSubject = "ceci est un sujet de mail";
 
     /**
      * méthode qui test le constructeur de la classe mail
      */
     private static void testConstructeur() {
         // on essaie de creer un mail
-        Mail mail1 = new Mail("Ceci est un test", "ceci est un sujet de mail");
+        new Mail(testContent, testSubject);
 
         // on essaie de creer un mail avec du text vide
-        Mail mail2 = new Mail("", "");
+        new Mail("", "");
 
         // on essaie de creer un mail avec des sauts de lignes
-        Mail mail3 = new Mail("Ceci est un /n test", "ceci est un sujet de mail");
+        new Mail("Ceci est un /n test", testSubject);
 
-        // on essaie de creer un mail avec un content == a null;
         try {
-            Mail mail4 = new Mail(null, null);
+            new Mail(null, null);
             System.err.println("Le contenu du mail peut etre null, alors que ce n'est pas le comportement attendu");
         } catch (IllegalArgumentException e) {
+            e.printStackTrace();
         }
     }
 
@@ -40,10 +42,9 @@ public class TestMail {
      * méthode qui test le getText() de la class mail
      */
     private static void testGetText() {
-        String content = "Ceci est un test";
-        String subject = "ceci est un sujet de mail";
+        String content = TestMail.testContent;
 
-        Mail mail1 = new Mail(content, subject);
+        Mail mail1 = new Mail(content, testSubject);
 
         if(! mail1.getText().equals(content)) {
             System.err.println("Le getText() renvoie : \"" + mail1.getText() + "\" alors qu'on attendait \"" + content + "\"...");
@@ -57,13 +58,12 @@ public class TestMail {
      * méthode qui test le getSubject() de la class mail
      */
     private static void testGetSubject() {
-        String content = "Ceci est un test";
-        String subject = "ceci est un sujet de mail";
+        String content = TestMail.testContent;
 
-        Mail mail1 = new Mail(content, subject);
+        Mail mail1 = new Mail(content, testSubject);
 
-        if(! mail1.getSubject().equals(subject)) {
-            System.err.println("Le getSubject() renvoie : \"" + mail1.getSubject() + "\" alors qu'on attendait \"" + subject + "\"...");
+        if(! mail1.getSubject().equals(testSubject)) {
+            System.err.println("Le getSubject() renvoie : \"" + mail1.getSubject() + "\" alors qu'on attendait \"" + testSubject + "\"...");
         } else {
             System.out.println("test getSubject() passe avec Succes");
         }
@@ -75,14 +75,14 @@ public class TestMail {
      * méthode qui test le getEtat() et setEtat() de la classe mail
      */
     private static void testGetSetEtat() {
-        String content = "Ceci est un test";
-        String subject = "ceci est un sujet de mail";
+        String content = TestMail.testContent;
         int nbSuccessfulTest = 0;
-        Mail mail1 = new Mail(content, subject);
+        final String ERR_SENTENCE = "Le getEtat() renvoie : [t1] alors qu'on attendait [t2]...";
+        Mail mail1 = new Mail(content, testSubject);
 
         // test non traitee par defaut
         if(! mail1.getState().equals(State.UNTREATED)) {
-            System.err.println("Le getEtat() renvoie : \"" + mail1.getState() + "\" alors qu'on attendait \"" + State.UNTREATED + "\"...");
+            System.err.println(ERR_SENTENCE.replace("[t1]", String.valueOf(mail1.getState())).replace( "[t2]", String.valueOf(State.UNTREATED)));
         } else {
             nbSuccessfulTest ++;
         }
@@ -90,7 +90,7 @@ public class TestMail {
         // test spam
         mail1.setState(State.SPAM);
         if(! mail1.getState().equals(State.SPAM)) {
-            System.err.println("Le getEtat() renvoie : \"" + mail1.getState() + "\" alors qu'on attendait \"" + State.SPAM + "\"...");
+            System.err.println(ERR_SENTENCE.replace("[t1]", String.valueOf(mail1.getState())).replace( "[t2]", String.valueOf(State.SPAM)));
         } else {
             nbSuccessfulTest ++;
         }
@@ -98,7 +98,7 @@ public class TestMail {
         // test non spam
         mail1.setState(State.NOT_SPAM);
         if(! mail1.getState().equals(State.NOT_SPAM)) {
-            System.err.println("Le getEtat() renvoie : \"" + mail1.getState() + "\" alors qu'on attendait \"" + State.NOT_SPAM + "\"...");
+            System.err.println(ERR_SENTENCE.replace("[t1]", String.valueOf(mail1.getState())).replace( "[t2]", String.valueOf(State.NOT_SPAM)));
         } else {
             nbSuccessfulTest ++;
         }
@@ -106,7 +106,7 @@ public class TestMail {
         // test non traitee
         mail1.setState(State.UNTREATED);
         if(! mail1.getState().equals(State.UNTREATED)) {
-            System.err.println("Le getEtat() renvoie : \"" + mail1.getState() + "\" alors qu'on attendait \"" + State.UNTREATED + "\"...");
+            System.err.println(ERR_SENTENCE.replace("[t1]", String.valueOf(mail1.getState())).replace( "[t2]", String.valueOf(State.UNTREATED)));
         } else {
             nbSuccessfulTest ++;
         }
@@ -119,15 +119,14 @@ public class TestMail {
      * méthode qui test le toString() de la classe mail
      */
     public static void testToString() {
-        String content = "Ceci est un test";
-        String subject = "ceci est un sujet de mail";
+        String content = TestMail.testContent;
 
-        Mail mail1 = new Mail(content, subject);
+        Mail mail1 = new Mail(content, testSubject);
 
-        if(mail1.toString() == subject) {
+        if(mail1.toString().equals(testSubject)) {
             System.out.println("test toString() passe avec Succes");
         } else {
-            System.err.println("Le toString() renvoie : \"" + mail1.toString() + "\" alors qu'on attendait \"" + subject + "\"...");
+            System.err.println("Le toString() renvoie : \"" + mail1.toString() + "\" alors qu'on attendait \"" + testSubject + "\"...");
         }
     }
 
