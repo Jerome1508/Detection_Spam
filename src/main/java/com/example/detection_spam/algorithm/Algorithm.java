8b12a3898c3ad57f5c3057ecc863ed63661679d2
Lethlos <romain.rp.palayert@gmail.com>
2023-04-02 15:57:41 +0200

    SonarLint : Algorythme

--- src/main/java/com/example/detection_spam/algorithm/Algorithm.java
+++ src/main/java/com/example/detection_spam/algorithm/Algorithm.java
@@ -11,6 +11,8 @@ import com.example.detection_spam.model.State;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Classe possédant plusieurs algorithmes nécessaire à l'apprentissage et à la détection de spam
@@ -18,6 +20,15 @@ import java.util.List;
  * @version 1.0.0
  */
 public class Algorithm {
+
+    private static Logger logger = Logger.getLogger(Algorithm.class.getPackage().getName());
+
+    /**
+     * Constructeur privé pour éviter de creer une instance de cette classe
+     */
+    private Algorithm() {
+        throw new IllegalStateException("Utility class");
+    }
 
     /**
      * Met à jour le dictionnaire pris en paramètre avec les données des mails pris en arguments.
@@ -32,7 +43,7 @@ public class Algorithm {
         // on parcourt les mails un par un
         for ( Mail mail: mails) {
 
-            List<String> encounteredWords = new ArrayList<String>();
+            List<String> encounteredWords = new ArrayList<>();
 
             if(spam) {
                 dicoWord.addSpamToLearn();
@@ -42,7 +53,7 @@ public class Algorithm {
 
             // on sépare les mots du mail
             String content = mail.getText();
-            String words[] = content.split(SPLITTER);
+            String[] words = content.split(SPLITTER);
 
             // on traite les mots un par un
             for (int i = 0; i < words.length; i++) {
@@ -76,7 +87,7 @@ public class Algorithm {
 
             // on sépare les mots du mail
             String content = mail.getText();
-            String words[] = content.split(SPLITTER);
+            String[] words = content.split(SPLITTER);
 
             // on traite les mots un par un
             for (int i = 0; i < words.length; i++) {
@@ -86,8 +97,12 @@ public class Algorithm {
                 }
             }
 
+            if(nbTreatableWord == 0) {
+                throw new ArithmeticException("Le mail ne contient aucun mot connu");
+            }
             probaMail = probaMail / nbTreatableWord;
-            System.out.println("Probabilite du mail : " + probaMail + "%");
+
+            logger.log( Level.INFO, String.format("Probabilite du mail : %d%",probaMail));
             if(probaMail > filter) {
                 mail.setState(State.SPAM);
             } else {
