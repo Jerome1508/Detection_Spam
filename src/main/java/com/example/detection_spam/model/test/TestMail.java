ff39c177b3bced03a53cb84cb6b356964a9c0a90
AlexisR3il <ravayroa@3il.fr>
2023-04-02 15:45:33 +0200

    Last fix SonarLint : TestMail

--- src/main/java/com/example/detection_spam/model/test/TestMail.java
+++ src/main/java/com/example/detection_spam/model/test/TestMail.java
@@ -18,21 +18,21 @@ import java.util.logging.Logger;
  */
 public class TestMail {
     private static Logger logger = Logger.getLogger(TestMail.class.getPackage().getName());
-    static final String testContent = "Ceci est un test";
-    static final String testSubject = "ceci est un sujet de mail";
+    static final String TEST_CONTENT = "Ceci est un test";
+    static final String TEST_SUBJECT = "ceci est un sujet de mail";
 
     /**
      * méthode qui test le constructeur de la classe mail
      */
     private static void testConstructeur() {
         // on essaie de creer un mail
-        new Mail(testContent, testSubject);
+        new Mail(TEST_CONTENT, TEST_SUBJECT);
 
         // on essaie de creer un mail avec du text vide
         new Mail("", "");
 
         // on essaie de creer un mail avec des sauts de lignes
-        new Mail("Ceci est un /n test", testSubject);
+        new Mail("Ceci est un /n test", TEST_SUBJECT);
 
         try {
             new Mail(null, null);
@@ -47,9 +47,9 @@ public class TestMail {
      * méthode qui test le getText() de la class mail
      */
     private static void testGetText() {
-        String content = TestMail.testContent;
+        String content = TestMail.TEST_CONTENT;
 
-        Mail mail1 = new Mail(content, testSubject);
+        Mail mail1 = new Mail(content, TEST_SUBJECT);
 
         if(! mail1.getText().equals(content)) {
             logger.log(Level.WARNING, String.format("Le getText() renvoie : %s alors qu'on attendait : %s", mail1.getText(), content));
@@ -63,12 +63,12 @@ public class TestMail {
      * méthode qui test le getSubject() de la class mail
      */
     private static void testGetSubject() {
-        String content = TestMail.testContent;
+        String content = TestMail.TEST_CONTENT;
 
-        Mail mail1 = new Mail(content, testSubject);
+        Mail mail1 = new Mail(content, TEST_SUBJECT);
 
-        if(! mail1.getSubject().equals(testSubject)) {
-            logger.log(Level.WARNING, String.format("Le getSubject() renvoie : %s  alors qu'on attendait : %s ..." , mail1.getSubject(), testSubject));
+        if(! mail1.getSubject().equals(TEST_SUBJECT)) {
+            logger.log(Level.WARNING, String.format("Le getSubject() renvoie : %s  alors qu'on attendait : %s ..." , mail1.getSubject(), TEST_SUBJECT));
         } else {
             logger.log(Level.INFO, "test getSubject() passe avec Succes");
         }
@@ -80,14 +80,14 @@ public class TestMail {
      * méthode qui test le getEtat() et setEtat() de la classe mail
      */
     private static void testGetSetEtat() {
-        String content = TestMail.testContent;
+        String content = TestMail.TEST_CONTENT;
         int nbSuccessfulTest = 0;
         final String ERR_SENTENCE = "Le getEtat() renvoie : [t1] alors qu'on attendait [t2]...";
-        Mail mail1 = new Mail(content, testSubject);
+        Mail mail1 = new Mail(content, TEST_SUBJECT);
 
         // test non traitee par defaut
         if(! mail1.getState().equals(State.UNTREATED)) {
-            logger.log( Level.WARNING, ERR_SENTENCE.replace("[t1]", String.valueOf(mail1.getState())).replace( "[t2]", String.valueOf(State.UNTREATED)));
+            logger.log(Level.WARNING, ERR_SENTENCE.replace("[t1]", String.valueOf(mail1.getState())).replace( "[t2]", String.valueOf(State.UNTREATED)));
         } else {
             nbSuccessfulTest ++;
         }
@@ -95,7 +95,7 @@ public class TestMail {
         // test spam
         mail1.setState(State.SPAM);
         if(! mail1.getState().equals(State.SPAM)) {
-            logger.log( Level.WARNING, ERR_SENTENCE.replace("[t1]", String.valueOf(mail1.getState())).replace( "[t2]", String.valueOf(State.SPAM)));
+            logger.log(Level.WARNING, ERR_SENTENCE.replace("[t1]", String.valueOf(mail1.getState())).replace( "[t2]", String.valueOf(State.SPAM)));
         } else {
             nbSuccessfulTest ++;
         }
@@ -103,7 +103,7 @@ public class TestMail {
         // test non spam
         mail1.setState(State.NOT_SPAM);
         if(! mail1.getState().equals(State.NOT_SPAM)) {
-            logger.log( Level.WARNING, ERR_SENTENCE.replace("[t1]", String.valueOf(mail1.getState())).replace( "[t2]", String.valueOf(State.NOT_SPAM)));
+            logger.log(Level.WARNING, ERR_SENTENCE.replace("[t1]", String.valueOf(mail1.getState())).replace( "[t2]", String.valueOf(State.NOT_SPAM)));
 
         } else {
             nbSuccessfulTest ++;
@@ -125,14 +125,14 @@ public class TestMail {
      * méthode qui test le toString() de la classe mail
      */
     public static void testToString() {
-        String content = TestMail.testContent;
+        String content = TestMail.TEST_CONTENT;
 
-        Mail mail1 = new Mail(content, testSubject);
+        Mail mail1 = new Mail(content, TEST_SUBJECT);
 
-        if(mail1.toString().equals(testSubject)) {
+        if(mail1.toString().equals(TEST_SUBJECT)) {
             logger.log(Level.INFO, "test toString() passe avec Succes");
         } else {
-            logger.log(Level.WARNING, String.format("Le toString() renvoie : %s alors qu'on attendait : %s", mail1.toString(), testSubject));
+            logger.log(Level.WARNING, String.format("Le toString() renvoie : %s alors qu'on attendait : %s", mail1.toString(), TEST_SUBJECT));
         }
     }
 
