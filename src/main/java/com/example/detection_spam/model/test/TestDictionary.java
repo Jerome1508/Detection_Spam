4bb9b679a535edbe3a2204fd5ca8f8f055cb910c
Lethlos <romain.rp.palayert@gmail.com>
2023-04-02 11:24:59 +0200

    SonarLint : testDictionary

--- src/main/java/com/example/detection_spam/model/test/TestDictionary.java
+++ src/main/java/com/example/detection_spam/model/test/TestDictionary.java
@@ -7,6 +7,12 @@ package com.example.detection_spam.model.test;
 
 import com.example.detection_spam.model.Dictionary;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  * Classe qui va effectuer les tests unitaires sur la classe Dictionnaire
  * @author Alexis RAVAYROL, Romain PALAYRET
@@ -14,20 +20,23 @@ import com.example.detection_spam.model.Dictionary;
  */
 public class TestDictionary {
 
+    private static Logger logger = Logger.getLogger(TestDictionary.class.getPackage().getName());
+
     /**
      * méthode qui test le constructeur de la classe dictionnaire
      */
     private static void testConstructor() {
         // on essaie de creer un Dictionnaire
-        Dictionary dico1 = new Dictionary();
+        new Dictionary();
     }
 
     /**
      * méthode qui test le getProbaSpam()
-     * Il s'agit d'une méthode privée, donc il faut penser à la mettre en public pour les tests.
+     * il s'agit d'une méthode privée, donc il faut penser à la mettre en public pour les tests.
      */
     private static void testgetProbaSpam() {
         int nbSuccessfulTest = 0;
+        final String WORD_TEST = "promo";
         Dictionary dico1 = new Dictionary();
 
         dico1.addSpamToLearn();
@@ -35,14 +44,14 @@ public class TestDictionary {
         dico1.addSpamToLearn();
         dico1.addNotSpamToLearn();
 
-        dico1.majProbaOrAdd("promo", true);
-        dico1.majProbaOrAdd("promo", true);
-        dico1.majProbaOrAdd("promo", false);
-        dico1.majProbaOrAdd("promo", true);
-        nbSuccessfulTest += dico1.getProbaSpam("promo") == 0.5 ? 1 : 0;
+        dico1.majProbaOrAdd(WORD_TEST, true);
+        dico1.majProbaOrAdd(WORD_TEST, true);
+        dico1.majProbaOrAdd(WORD_TEST, false);
+        dico1.majProbaOrAdd(WORD_TEST, true);
+        nbSuccessfulTest += dico1.getProbaSpam(WORD_TEST) == 0.5 ? 1 : 0;
         nbSuccessfulTest += dico1.getProbaSpam("mot inconnu") == 0 ? 1 : 0;
 
-        System.out.println("getProbaSpam() : Test reussi => " + nbSuccessfulTest + "/2");
+        logger.log( Level.INFO, "getProbaSpam() : Test reussi => " + nbSuccessfulTest + "/2");
 
     }
 
@@ -51,6 +60,7 @@ public class TestDictionary {
      */
     private static void testgetProbaNotSpam() {
         int nbSuccessfulTest = 0;
+        final String WORD_TEST = "promo";
         Dictionary dico1 = new Dictionary();
 
         dico1.addSpamToLearn();
@@ -60,15 +70,15 @@ public class TestDictionary {
         dico1.addNotSpamToLearn();
         dico1.addNotSpamToLearn();
 
-        dico1.majProbaOrAdd("promo", true);
-        dico1.majProbaOrAdd("promo", true);
-        dico1.majProbaOrAdd("promo", false);
-        dico1.majProbaOrAdd("promo", true);
+        dico1.majProbaOrAdd(WORD_TEST, true);
+        dico1.majProbaOrAdd(WORD_TEST, true);
+        dico1.majProbaOrAdd(WORD_TEST, false);
+        dico1.majProbaOrAdd(WORD_TEST, true);
 
-        nbSuccessfulTest += dico1.getProbaNotSpam("promo") == 0.25 ? 1 : 0;
+        nbSuccessfulTest += dico1.getProbaNotSpam(WORD_TEST) == 0.25 ? 1 : 0;
         nbSuccessfulTest += dico1.getProbaNotSpam("mot inconnu") == 0 ? 1 : 0;
 
-        System.out.println("getProbaNotSpam() : Test reussi => " + nbSuccessfulTest + "/2");
+        logger.log( Level.INFO, "getProbaNotSpam() : Test reussi => " + nbSuccessfulTest + "/2");
     }
 
     /**
@@ -80,50 +90,59 @@ public class TestDictionary {
         int nbSuccessfulTest = 0;
         Dictionary dico1 = new Dictionary();
 
+        List<String> wordsTest = new ArrayList<>();
+        wordsTest.add("test1");
+        wordsTest.add("test2");
+        wordsTest.add("test3");
+        wordsTest.add("test4");
+        wordsTest.add("test5");
+        wordsTest.add("test6");
+        wordsTest.add("test7");
+
         // Test 1 : on ajoute un mot spam
-        dico1.majProbaOrAdd("test1", true);
-        nbSuccessfulTest += dico1.getProbaSpam("test1") == 1 ? 1 : 0;
-        System.out.println(dico1.getProbaSpam("test1"));
-        nbSuccessfulTest += dico1.getProbaNotSpam("test1") == 0 ? 1 : 0;
+        dico1.majProbaOrAdd(wordsTest.get(0), true);
+        nbSuccessfulTest += dico1.getProbaSpam(wordsTest.get(0)) == 1 ? 1 : 0;
+        System.out.println(dico1.getProbaSpam(wordsTest.get(0)));
+        nbSuccessfulTest += dico1.getProbaNotSpam(wordsTest.get(0)) == 0 ? 1 : 0;
 
         // Test 2 : on ajoute un mot non spam
-        dico1.majProbaOrAdd("test2", false);
-        nbSuccessfulTest += dico1.getProbaNotSpam("test2") == 1 ? 1 : 0;
-        nbSuccessfulTest += dico1.getProbaSpam("test2") == 0 ? 1 : 0;
+        dico1.majProbaOrAdd(wordsTest.get(1), false);
+        nbSuccessfulTest += dico1.getProbaNotSpam(wordsTest.get(1)) == 1 ? 1 : 0;
+        nbSuccessfulTest += dico1.getProbaSpam(wordsTest.get(1)) == 0 ? 1 : 0;
 
         // Test 3 : on ajoute un mot spam, et on lui met a jour sa proba spam
-        dico1.majProbaOrAdd("test3", true);
-        dico1.majProbaOrAdd("test3", true);
-        nbSuccessfulTest += dico1.getProbaSpam("test3") == 1 ? 1 : 0;
-        nbSuccessfulTest += dico1.getProbaNotSpam("test3") == 0 ? 1 : 0;
+        dico1.majProbaOrAdd(wordsTest.get(2), true);
+        dico1.majProbaOrAdd(wordsTest.get(2), true);
+        nbSuccessfulTest += dico1.getProbaSpam(wordsTest.get(2)) == 1 ? 1 : 0;
+        nbSuccessfulTest += dico1.getProbaNotSpam(wordsTest.get(2)) == 0 ? 1 : 0;
 
         // Test 4 : on ajoute un mot spam, et on lui met a jour sa proba non spam
-        dico1.majProbaOrAdd("test4", true);
-        dico1.majProbaOrAdd("test4", false);
-        nbSuccessfulTest += dico1.getProbaSpam("test4") == 0.5 ? 1 : 0;
-        nbSuccessfulTest += dico1.getProbaNotSpam("test4") == 0.5 ? 1 : 0;
+        dico1.majProbaOrAdd(wordsTest.get(3), true);
+        dico1.majProbaOrAdd(wordsTest.get(3), false);
+        nbSuccessfulTest += dico1.getProbaSpam(wordsTest.get(3)) == 0.5 ? 1 : 0;
+        nbSuccessfulTest += dico1.getProbaNotSpam(wordsTest.get(3)) == 0.5 ? 1 : 0;
 
         // Test 5 : on ajoute un mot non spam, et on lui met a jour sa proba non spam
-        dico1.majProbaOrAdd("test5", false);
-        dico1.majProbaOrAdd("test5", false);
-        nbSuccessfulTest += dico1.getProbaSpam("test5") == 0 ? 1 : 0;
-        nbSuccessfulTest += dico1.getProbaNotSpam("test5") == 1 ? 1 : 0;
+        dico1.majProbaOrAdd(wordsTest.get(4), false);
+        dico1.majProbaOrAdd(wordsTest.get(4), false);
+        nbSuccessfulTest += dico1.getProbaSpam(wordsTest.get(4)) == 0 ? 1 : 0;
+        nbSuccessfulTest += dico1.getProbaNotSpam(wordsTest.get(4)) == 1 ? 1 : 0;
 
         // Test 6 : on ajoute un mot non spam, et on lui met a jour sa proba spam
-        dico1.majProbaOrAdd("test6", false);
-        dico1.majProbaOrAdd("test6", true);
-        nbSuccessfulTest += dico1.getProbaSpam("test6") == 0.5 ? 1 : 0;
-        nbSuccessfulTest += dico1.getProbaNotSpam("test6") == 0.5 ? 1 : 0;
+        dico1.majProbaOrAdd(wordsTest.get(5), false);
+        dico1.majProbaOrAdd(wordsTest.get(5), true);
+        nbSuccessfulTest += dico1.getProbaSpam(wordsTest.get(5)) == 0.5 ? 1 : 0;
+        nbSuccessfulTest += dico1.getProbaNotSpam(wordsTest.get(5)) == 0.5 ? 1 : 0;
 
         // Test 7 : on ajoute un mot , et on lui met a jour sa proba plusieurs fois
-        dico1.majProbaOrAdd("test7", true);
-        dico1.majProbaOrAdd("test7", true);
-        dico1.majProbaOrAdd("test7", false);
-        dico1.majProbaOrAdd("test7", true);
-        nbSuccessfulTest += dico1.getProbaSpam("test7") == 0.75 ? 1 : 0;
-        nbSuccessfulTest += dico1.getProbaNotSpam("test7") == 0.25 ? 1 : 0;
+        dico1.majProbaOrAdd(wordsTest.get(6), true);
+        dico1.majProbaOrAdd(wordsTest.get(6), true);
+        dico1.majProbaOrAdd(wordsTest.get(6), false);
+        dico1.majProbaOrAdd(wordsTest.get(6), true);
+        nbSuccessfulTest += dico1.getProbaSpam(wordsTest.get(6)) == 0.75 ? 1 : 0;
+        nbSuccessfulTest += dico1.getProbaNotSpam(wordsTest.get(6)) == 0.25 ? 1 : 0;
 
-        System.out.println("majProbaOrAdd() : Test reussi => " + nbSuccessfulTest + "/14");
+        logger.log( Level.INFO, "majProbaOrAdd() : Test reussi => " + nbSuccessfulTest + "/14");
 
     }
 
@@ -134,22 +153,31 @@ public class TestDictionary {
      */
     private static void testSerialisable() {
 
+        List<String> wordsTest = new ArrayList<>();
+        wordsTest.add("test1");
+        wordsTest.add("test2");
+        wordsTest.add("test3");
+        wordsTest.add("test4");
+        wordsTest.add("test5");
+        wordsTest.add("test6");
+        wordsTest.add("test7");
+
         // on crée un dictionnaire
         Dictionary dico1 = new Dictionary();
-        dico1.majProbaOrAdd("test1", true);
-        dico1.majProbaOrAdd("test2", false);
-        dico1.majProbaOrAdd("test3", true);
-        dico1.majProbaOrAdd("test3", true);
-        dico1.majProbaOrAdd("test4", true);
-        dico1.majProbaOrAdd("test4", false);
-        dico1.majProbaOrAdd("test5", false);
-        dico1.majProbaOrAdd("test5", false);
-        dico1.majProbaOrAdd("test6", false);
-        dico1.majProbaOrAdd("test6", true);
-        dico1.majProbaOrAdd("test7", true);
-        dico1.majProbaOrAdd("test7", true);
-        dico1.majProbaOrAdd("test7", false);
-        dico1.majProbaOrAdd("test7", true);
+        dico1.majProbaOrAdd(wordsTest.get(0), true);
+        dico1.majProbaOrAdd(wordsTest.get(1), false);
+        dico1.majProbaOrAdd(wordsTest.get(2), true);
+        dico1.majProbaOrAdd(wordsTest.get(2), true);
+        dico1.majProbaOrAdd(wordsTest.get(3), true);
+        dico1.majProbaOrAdd(wordsTest.get(3), false);
+        dico1.majProbaOrAdd(wordsTest.get(4), false);
+        dico1.majProbaOrAdd(wordsTest.get(4), false);
+        dico1.majProbaOrAdd(wordsTest.get(5), false);
+        dico1.majProbaOrAdd(wordsTest.get(5), true);
+        dico1.majProbaOrAdd(wordsTest.get(6), true);
+        dico1.majProbaOrAdd(wordsTest.get(6), true);
+        dico1.majProbaOrAdd(wordsTest.get(6), false);
+        dico1.majProbaOrAdd(wordsTest.get(6), true);
 
         dico1.addSpamToLearn();
         dico1.addSpamToLearn();
@@ -173,7 +201,7 @@ public class TestDictionary {
         nbSuccessfulTest += dico2.getProbaSpam("test7") == 0.75 ? 1 : 0;
         nbSuccessfulTest += dico2.getNbSpamToLearn() == 3 ? 1 : 0;
 
-        System.out.println(nbSuccessfulTest == 2 ? "de-serialisation reussie" : "echec");
+        logger.log( Level.INFO, nbSuccessfulTest == 2 ? "de-serialisation reussie" : "echec");
     }
 
     /**
@@ -187,7 +215,7 @@ public class TestDictionary {
         dico1.addSpamToLearn();
         nbSuccessfulTest += dico1.getNbSpamToLearn() == 1 ? 1 : 0;
 
-        System.out.println("getNbSpamToLearn() : Test reussi => " + nbSuccessfulTest + "/2");
+        logger.log( Level.INFO, "getNbSpamToLearn() : Test reussi => " + nbSuccessfulTest + "/2");
     }
 
     /**
@@ -201,7 +229,7 @@ public class TestDictionary {
         dico1.addNotSpamToLearn();
         nbSuccessfulTest += dico1.getNbNotSpamToLearn() == 1 ? 1 : 0;
 
-        System.out.println("getNbNotSpamToLearn() : Test reussi => " + nbSuccessfulTest + "/2");
+        logger.log( Level.INFO, "getNbNotSpamToLearn() : Test reussi => " + nbSuccessfulTest + "/2");
     }
 
     /**
@@ -216,7 +244,7 @@ public class TestDictionary {
         nbSuccessfulTest += dico1.containsWord("Bonjour") ? 1 : 0;
         nbSuccessfulTest += dico1.containsWord("Philippe") ? 0 : 1;
 
-        System.out.println("containsWord() : Test reussi => " + nbSuccessfulTest + "/2");
+        logger.log( Level.INFO, "containsWord() : Test reussi => " + nbSuccessfulTest + "/2");
     }
 
     /**
@@ -224,14 +252,14 @@ public class TestDictionary {
      * @param args
      */
     public static void main (String[] args) {
-        //testConstructor();
-        //testgetProbaSpam();
-        //testgetProbaNotSpam();
-        //testMajProbaOrAdd();
+        testConstructor();
+        testgetProbaSpam();
+        testgetProbaNotSpam();
+        testMajProbaOrAdd();
         testSerialisable();
-        //testDeserialisable();
-        //testGetAddNbSpamToLearn();
-        //testGetAddNbNotSpamToLearn();
-        //testContainsWord();
+        testDeserialisable();
+        testGetAddNbSpamToLearn();
+        testGetAddNbNotSpamToLearn();
+        testContainsWord();
     }
 }
